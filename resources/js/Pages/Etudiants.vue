ccc654d446289edbcfad78e12ad79fbefeef7c70
Mohamed Vall Bah <vallmed550@gmail.com>
2025-12-16 18:05:38 +0000

    Sprint 1 : Ajout du module Etudiants

---
+++ resources/js/Pages/Etudiants.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container mt-5">
+        <h2>Liste des étudiants</h2>
+
+        <table border="1" cellpadding="8">
+            <thead>
+                <tr>
+                    <th>Matricule</th>
+                    <th>Nom</th>
+                    <th>Prénom</th>
+                    <th>Filière</th>
+                    <th>Niveau</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="etudiant in etudiants" :key="etudiant.id">
+                    <td>{{ etudiant.matricule }}</td>
+                    <td>{{ etudiant.nom }}</td>
+                    <td>{{ etudiant.prenom }}</td>
+                    <td>{{ etudiant.filiere }}</td>
+                    <td>{{ etudiant.niveau }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    etudiants: Array
+})
+</script>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\EtudiantController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('etudiants', EtudiantController::class);
